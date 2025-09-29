226b5fabca063f25ce75167969668badf5f76e01
GynGo-AST <augusto.schaumburg@gmail.com>
2025-09-29 19:02:07 -0300

    subida load json

--- load_json/main.py
+++ load_json/main.py
@@ -1,21 +1,23 @@
-import json
+""" importacion de modulos"""
 import requests
+import json
 
 
 path_disco = "H:/INET-cisco/db_bases"
 nombre_archivo = "provincias.json"
 path_archivo = f"{path_disco}/{nombre_archivo}"
+
 # Abre el archivo JSON en modo lectura ('r')
 with open(path_archivo, 'r') as archivo_json:
     # Carga el contenido del archivo JSON en una variable Python
     datos = json.load(archivo_json)
 
 # Ahora puedes trabajar con los datos como un diccionario o lista de Python
-#print(type(datos))
-    
+# print(type(datos))
+
 print(datos["provincias"][0])
 
 
-x = requests.get('https://w3schools.com/python/demopage.htm')
+respuesta = requests.get('https://w3schools.com/python/demopage.htm')
 
-print(x.text)
+print(respuesta.text)
